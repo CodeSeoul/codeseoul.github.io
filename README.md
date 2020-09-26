97eeacf4dc551ab1c9f3df8ab06420ba578bad6c
Josie Daw <jdaw1@hotmail.co.uk>
2020-09-26 18:40:26 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# CODESEOUL
+This project will be used as the basis for the CodeSeoul website. 
+
+## Dependencies 
+- Bootstrap 
+
+## Usage
+-> npm install
+-> npm start
---
+++ js/scripts.js
@@ -0,0 +1,103 @@
+/**
+ * Smooth Scrolling
+ */
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 56)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 56
+  });
+
+})(jQuery); // End of use strict
+
+
+
+/**
+ * Meetup API
+ */
+
+function getMeetupEvents() {
+  $.ajax({
+    url: "https://api.meetup.com/CodeSeoul/events",
+    jsonp: "callback",
+    dataType: "jsonp",
+    data: {
+      format: "json"
+    },
+    success: function(response) {
+      let events = response.data;
+      createMeetupEvents(events);
+    }
+  });
+}
+getMeetupEvents();
+
+function createMeetupEvents(data) {
+  let eventsDiv = document.querySelector(`#events-container`);
+
+  console.log(data[0])
+
+  for(let i =0; i< data.length; i++){
+
+    let eventItem = data[i];
+
+    //Create card
+    let eventCard = document.createElement('div');
+    eventCard.setAttribute('class', 'card');
+
+    //Create card body
+    let cardBody = document.createElement('div');
+    cardBody.setAttribute('class', 'card-body');
+    eventCard.appendChild(cardBody);
+
+    //Create card title
+    let cardTitle = document.createElement('h4');
+    cardTitle.setAttribute('class', 'card-title');
+    cardTitle.innerHTML = eventItem['name'];
+    cardBody.appendChild(cardTitle);
+    
+    //Create card text
+    let cardText = document.createElement('p');
+    cardText.setAttribute('class', 'card-text');
+    cardText.innerHTML = 'Date: ' + eventItem['local_date'] + '<br/>' + 'Time: ' + eventItem['local_time'];
+    cardBody.appendChild(cardText);
+
+    //Create location text
+    let cardTextLocation = document.createElement('p');
+    cardTextLocation.setAttribute('class', 'card-text');
+    cardTextLocation.innerHTML = 'Location: ' + eventItem['venue']['name'] + '<br/>' + eventItem['venue']['address_1'];
+    cardBody.appendChild(cardTextLocation);
+
+    //Create Link Button
+    let cardButton = document.createElement('a');
+    cardButton.setAttribute('class', 'btn btn-blue stretched-link');
+    cardButton.setAttribute('href', eventItem['link']);
+    cardButton.innerHTML = 'More Info';
+    cardBody.appendChild(cardButton);
+
+    eventsDiv.appendChild(eventCard);
+  }
+
+
+}
