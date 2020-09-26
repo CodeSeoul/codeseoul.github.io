b2a19b4824da837a535b940a407077a1dfbdc83a
Josie Daw <jdaw1@hotmail.co.uk>
2020-09-26 20:35:09 +0900

    Updated style, fix jump scroll

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,7 @@
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: (target.offset().top - 56)
+          scrollTop: (target.offset().top - 100)
         }, 1000, "easeInOutExpo");
         return false;
       }
